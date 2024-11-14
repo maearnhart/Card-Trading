d3dda93e1eba63a12e53c189147f08b3214afb2c
uduttadev <dutta.utsav14@gmail.com>
2024-11-14 14:16:32 -0900

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,9 +1,10 @@
 #include <string>
 
 struct Card{
-string name;
-string owner;
-int value;
+  string name;
+  string owner;
+  int value;
+};
 
 Card(string Name, string Owner, int val) : name(Name), owner(Owner), value(val) {} //Constructor
 };
