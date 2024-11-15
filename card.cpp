00004d1348f386feb666aa8c43042e41f44106c0
uduttadev <dutta.utsav14@gmail.com>
2024-11-15 04:32:04 -0900

    Update card.cpp
    Fleshed out print function,, however we may not need it depending on later direction

--- card.cpp
+++ card.cpp
@@ -1,12 +1,19 @@
 #include "Card.h"
+#include "UI.h"
+
+#include <iostream>
+#include <set>
 
 using namespace std;
 
-void Card::Print(Card)
+void Card::print()
 {
-  cout << "Name: " << Card.name << endl;
-  cout << "Value: " << Card.value << endl;
-  cout << "Owned by: " << Card.owner << endl;
-  cout << endl;
-  cout << endl;
+	// Notes: Uses "\n" instead of endl to create newlines as endl performs flushing operations on the output stream
+	cout << "Name: " << this->name << "\n"
+		<< "Value: " << this->value << "\n"
+		<< "Owned by: " << this->owner << endl;
+}
+
+void Card::print(const set<Card*> cards) {
+	UIOut("Cards", true, cards);
 }
