08fddeb5912cbbeea0f8c23933daa0976a01e5df
uduttadev <dutta.utsav14@gmail.com>
2024-11-15 04:31:19 -0900

    Update CardDatabase.h
    Added display functions, some function overloads as well

--- CardDatabase.h
+++ CardDatabase.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "Card.h"
 #include <iostream>
 #include <vector>
@@ -7,22 +9,26 @@
 
 using namespace std;
 
-class CardDatabase{
-public:
-  CardDatabase();
-  ~CardDatabase();
-  void addCard(Card* card);
-  void addCard(string name, string owner, int val); // Function Overload
-  //Card* getCard(int n);
-  set<Card*> getCards(set<int> cards); // Function Overload
-  set<int> searchName(string name);
-  set<int> searchVal(int val);
-  set<int> searchOwner(string name);
-  void displayDatabase();
-
+class CardDatabase {
 private:
-  vector<Card*> Deck;
-  map<string, int> nameToSlot;
-  map<int, set<int>> valueToSlot;
-  map<string, set<int>> ownerToSlot;
+	vector<Card*> deck;
+	map<string, set<int>> nameToSlot;
+	map<int, set<int>> valueToSlot;
+	map<string, set<int>> ownerToSlot;
+
+public:
+	CardDatabase();
+	void addCard(Card* card);
+	void addCard(string name, string owner, int val); // Function Overload
+
+	set<Card*> getCards(int n);
+	set<Card*> getCards(set<int> cards);
+
+	set<int> searchName(string name);
+	set<int> searchVal(string op, int val);
+	set<int> searchOwner(string name);
+	void displayDatabase();
+	void displayOwner(string owner);
+	void displayCards(set<int>);
+
 };
