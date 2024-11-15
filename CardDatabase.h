c27bfa7fcfb958a92f56f58b41921412df865d35
uduttadev <dutta.utsav14@gmail.com>
2024-11-15 00:01:18 +0000

    Edited Database.h

--- CardDatabase.h
+++ CardDatabase.h
@@ -13,14 +13,15 @@ public:
   ~CardDatabase();
   void addCard(Card* card);
   void addCard(string name, string owner, int val); // Function Overload
-  Card getCard(int n);
+  Card* getCard(int n);
+  Card* getCard(set<int>); // Function Overload
   int searchName(string name);
-  int searchVal(int val);
-  int searchOwner(string name);
+  set<int> searchVal(int val);
+  set<int> searchOwner(string name);
   void displayDatabase();
 
 private:
-  vector<Card> cards;
+  vector<Card*> cards;
   map<string, int> nameToSlot;
   map<int, set<int>> valueToSlot;
   map<string, set<int>> ownerToSlot;
